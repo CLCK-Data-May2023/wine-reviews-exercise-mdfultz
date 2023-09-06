dbd4cd0cab23d1711f4acd6d3f4293238482023b
mdfultz <129699601+mdfultz@users.noreply.github.com>
2023-09-05 20:00:39 -0400

    test1

--- reviews.py
+++ reviews.py
@@ -1,2 +1,14 @@
-# add your code here
+import pandas as pd
+import numpy as np
 
+winereview = pd.read_csv("data/winemag-data-130k-v2.csv.zip", index_col=0)
+
+count = winereview.country.value_counts()
+
+points_avg = winereview.groupby('country')['points'].mean().round(1)
+
+df = pd.DataFrame.merge(count, points_avg, on = 'country')
+
+print(df)
+
+df.to_csv('data/reviews-per-country.csv')
